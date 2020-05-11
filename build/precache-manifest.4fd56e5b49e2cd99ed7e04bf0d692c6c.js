bfc8d7d84ba8bf143dccf82e305d322a6a388d3f
webdevjosh <ozzikid@gmail.com>
2020-05-11 17:51:07 -0400

    fixed incontent queue remove bug

--- build/precache-manifest.4fd56e5b49e2cd99ed7e04bf0d692c6c.js
+++ build/precache-manifest.4fd56e5b49e2cd99ed7e04bf0d692c6c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5a457feb21f5f1ffcaffc75814bad369",
+    "revision": "36eeb660479b37eb33ff9d2b6ca529f0",
     "url": "/index.html"
   },
   {
-    "revision": "860b7c3d38c116f5b34c",
-    "url": "/static/css/main.0e642fd5.chunk.css"
+    "revision": "199d84541804b1007170",
+    "url": "/static/css/main.b7b609fe.chunk.css"
   },
   {
     "revision": "50b6a918e8a9099202b6",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.f74bb90d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "860b7c3d38c116f5b34c",
-    "url": "/static/js/main.ee6f5fe9.chunk.js"
+    "revision": "199d84541804b1007170",
+    "url": "/static/js/main.b6ba7811.chunk.js"
   },
   {
     "revision": "ea0d3bf38a38abcf26f2",
